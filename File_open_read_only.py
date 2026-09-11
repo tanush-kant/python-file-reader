706cc50dcd71fd3dd9a77301af8d8b601da322bd
Tanush Kant <tanush.kant.dev@gmail.com>
2026-09-11 18:43:31 +0530

    Add files via upload

---
+++ File_open_read_only.py
@@ -0,0 +1,8 @@
+try:
+    with open("example", "r") as f: # Your file will come here in that example place just replace it with your file name with its format and your good to go
+        print(f.read())
+except Exception as e:
+    print(e)
+ 
+# End of the code
+# This code is made by Tanush kant 
